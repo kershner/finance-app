7cd86bcf1b0cf178afbb5e9edae4cc4a6446d9cc
Tyler <tylerkershner@gmail.com>
2022-07-09 21:41:31 -0400

    move secret key to settings file since it's not important here

--- app/settings.py
+++ app/settings.py
@@ -18,13 +18,13 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-PARAMETERS = util.get_parameters()
+# PARAMETERS = util.get_parameters()
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = PARAMETERS['SECRET_KEY']
+SECRET_KEY = "not-important-for-this-app-since-it-only-runs-locally"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
